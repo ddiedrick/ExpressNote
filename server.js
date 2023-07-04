33781162b08042d0660730f8f36b1ee251ae2d58
deandradiedrick <dadiedrick@gmail.com>
2023-07-03 22:52:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const noteData = require('./db/db.json');
-const PORT = 3001;
+const PORT = 3002;
 const apiRoutes = require('./routes/apiRoutes');
 const homeRoutes = require('./routes/homeRoute')
 
